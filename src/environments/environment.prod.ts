1f186523d5506998de5b27d8b5f1110456744d08
Anibal <anibal.alvarezg@gmail.com>
2025-10-23 20:37:06 -0300

    Add enviroment fields

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  api: {
+    url: 'https://beer9.p.rapidapi.com/',
+    host: 'beer9.p.rapidapi.com',
+    key: 'f817aae9ccmshcf30d75c746cb38p158521jsn948bf5b7942d' // Tu clave
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,10 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  api: {
+    url: 'https://beer9.p.rapidapi.com/',
+    host: 'beer9.p.rapidapi.com',
+    key: 'f817aae9ccmshcf30d75c746cb38p158521jsn948bf5b7942d'
+  }
 };
