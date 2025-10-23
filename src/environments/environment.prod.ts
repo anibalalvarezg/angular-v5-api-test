eee4cb1067744245f593b5f74e582ad4279535e9
Anibal <anibal.alvarezg@gmail.com>
2025-10-23 20:39:57 -0300

    Format code

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   api: {
     url: 'https://beer9.p.rapidapi.com/',
     host: 'beer9.p.rapidapi.com',
-    key: 'f817aae9ccmshcf30d75c746cb38p158521jsn948bf5b7942d' // Tu clave
-  }
+    key: 'f817aae9ccmshcf30d75c746cb38p158521jsn948bf5b7942d', // Tu clave
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,6 +8,6 @@ export const environment = {
   api: {
     url: 'https://beer9.p.rapidapi.com/',
     host: 'beer9.p.rapidapi.com',
-    key: 'f817aae9ccmshcf30d75c746cb38p158521jsn948bf5b7942d'
-  }
+    key: 'f817aae9ccmshcf30d75c746cb38p158521jsn948bf5b7942d',
+  },
 };
