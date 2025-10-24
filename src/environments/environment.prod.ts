024e073e934fe5eb6dfb36a9a0085e08f05919b5
Anibal <anibal.alvarezg@gmail.com>
2025-10-23 22:01:26 -0300

    update apikey

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   api: {
     url: 'https://beer9.p.rapidapi.com/',
     host: 'beer9.p.rapidapi.com',
-    key: 'f817aae9ccmshcf30d75c746cb38p158521jsn948bf5b7942d', // Tu clave
+    key: '7ed4cb4acfmsh739b18c4b85ba8cp1d2b88jsn12d1a',
   },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,6 +8,6 @@ export const environment = {
   api: {
     url: 'https://beer9.p.rapidapi.com/',
     host: 'beer9.p.rapidapi.com',
-    key: 'f817aae9ccmshcf30d75c746cb38p158521jsn948bf5b7942d',
+    key: '7ed4cb4acfmsh739b18c4b85ba8cp1d2b88jsn12d1a',
   },
 };
