a8719612b0b58cd4093be264a992dba6f3cafeb1
Anibal <anibal.alvarezg@gmail.com>
2025-10-23 22:20:13 -0300

    Update app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/take';
+
+import { selectIsLoading } from './store/ui.selectors';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'Mi App de Cervezas v5';
+  isLoading$: Observable<boolean>;
+
+  constructor(private store: Store<any>) {}
+
+  ngOnInit() {
+    this.isLoading$ = this.store.select(selectIsLoading);
+  }
 }
