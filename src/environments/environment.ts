e9ad0db33c6e5b8b335b55af91d9afde30d32678
Anibal <anibal.alvarezg@gmail.com>
2025-11-03 19:21:35 -0300

    Remove API key from environment configuration for production and development environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,6 +8,6 @@ export const environment = {
   api: {
     url: 'https://beer9.p.rapidapi.com/',
     host: 'beer9.p.rapidapi.com',
-    key: '7ed4cb4acfmsh739b18c4b85ba8cp1d2b88jsn12d1af6f61a5',
+    key: '',
   },
 };
